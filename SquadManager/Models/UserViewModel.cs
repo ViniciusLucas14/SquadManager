7d214ab4b15bc64f17242c8635dd655e4b131d8e
Vinicius Lucas <vlucassouza@gmail.com>
2022-02-17 23:31:02 -0300

    Model/View/Controller/ Login

---
+++ SquadManager/Models/UserViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SquadManager.Models
+{
+    public class UserViewModel
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }       
+        public string Password { get; set; }    
+    }
+}
